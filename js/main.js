10ae77d5b0333df52daa6ff254a423523f42413d
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-30 23:07:20 +0500

    2-masala

--- js/main.js
+++ js/main.js
@@ -6,5 +6,14 @@
 //     console.log("k");
 // }
 
+// 2-Masala;
+
+// let a = prompt("kg ni kiriting");
+
+// for (let i = 0; i < a; i+=0.1) {
+//     yigindi = 3000;
+//     sum = yigindi+=i;
+//     console.log(sum);
+// }
 
 
