08ac1baa784e1767e8978a5a582460d4ca56307f
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-02 22:45:15 +0500

    sa

--- js/main.js
+++ js/main.js
@@ -104,3 +104,4 @@
 //  n butun soni berilgan (n > 0). Quyidagi ko'paytmani hisoblovchi programma tuzilsin. S=1.1*1.2* 1.3*... *n
 // n butun soni berilgan (n > 0). Shu sonning kvadratini quyidagi formula asosida hisoblovchi programma tuzilsin.
 // n^2=1+3+5+... + (2*n-1)
+// Har bir qo'shiluvchidan keyin natijani ekranga chiqarib boring. Natijda ekranda 1 dan n gacha bo'lgan sonlar kvadrati chiqariladi.
