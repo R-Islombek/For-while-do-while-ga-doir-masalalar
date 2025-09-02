fb88a90ae80b3806064f9fa2a979f52c89ca0128
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-02 22:34:58 +0500

    4-masala

--- js/main.js
+++ js/main.js
@@ -29,3 +29,14 @@
 // }
 
 // console.log("Yig'indi:", S);
+
+// 4-Masala;
+
+let n = prompt("n sonini kiriting"); 
+let S = 0;
+
+for (let i = 1; i <= n; i++) {
+  S += i ** (n - i + 1);
+}
+
+console.log("Yig'indi:", S);
