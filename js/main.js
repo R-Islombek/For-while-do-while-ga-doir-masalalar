b65f8de5af32d61c6df838e1a204bbe57fc75ca8
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-04 23:53:54 +0500

    sasasasas

--- js/main.js
+++ js/main.js
@@ -128,3 +128,5 @@
 
 
 // Begin14. X kg shokolad A so`m turadi va Y kg konfet B so`m turadi. 1 kg shokolad 1 kg konfetdan qancha qimmat turishini aniqlovchi programma tuzilsin.
+
+// Abdulaziz X kg olmani A so’mdan, Y kg olmani B so’mdan sotib oldi. Abdulaziz jami qancha savdo qilgan?
