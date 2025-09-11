cbff166dd3ce3adb4d58f5715cf96a896d447062
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-09-11 09:29:35 +0500

    2-masala

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@
 // for (let i = 0; i < n; i++){
 //       console.log("k");
 // }
+
+// 2-Masala;
+
+// let n = prompt("0 bilan 1 bilan orasidagi son kiriting");
+
+// let sum = 200;
+
+// for( i = 0.1; i < n ; i ++){
+//     sum *= i;
+//     console.log(sum);
+// }
+
